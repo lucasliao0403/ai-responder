3ca54fb33eac12fdb74318349a1ab29ae41e8ae0
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2023-09-17 00:48:39 -0400

    linked frontend with backend API, reformatted cards

--- pages/constants.js
+++ pages/constants.js
@@ -5,6 +5,7 @@ export const calls = [ // TEMPORARY: ADD IN CALLS LATER
         time: "13:01:02",
         location: "Park Village",
         id: "test",
+        emergency: "fire",
         log: [
         {
             time: "13:01:03",
@@ -43,6 +44,7 @@ export const calls = [ // TEMPORARY: ADD IN CALLS LATER
         time: "15:02:35",
         location: "Ya Muthas House",
         id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+        emergency: "water",
         log: [
             {
                 time: "13:01:03",
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useState } from 'react'
 import { motion } from "framer-motion"
-import {calls} from './constants.js'
+import {calls} from './constants.js';
+import { MdCall } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import io from "socket.io-client";
+import PriorityCards from "./PriorityCards";
+import { liveData, automatedData } from "./data/data";
+
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +20,39 @@ const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
+
+    const [automated, setautomated] = useState(true);
+    const [selectedCard, setSelectedCard] = useState("efnwhfwhn1");
+    const [data, setData] = useState(liveData);
+    const [priorityCard, setpriorityCard] = useState(automatedData);
+    let labels = ["1", "2", "3", "4", "5"];
+
+    const updateLabel = (e) => {
+        let theCards = [];
+    
+        automated
+          ? priorityCard.forEach((card) => {
+              if (card.id == selectedCard) {
+                theCards.push({
+                  ...card,
+                  priority: parseInt(e.target.value),
+                });
+              } else {
+                theCards.push(card);
+              }
+            })
+          : data.forEach((card) => {
+              if (card.id == selectedCard) {
+                theCards.push({
+                  ...card,
+                  priority: parseInt(e.target.value),
+                });
+              } else {
+                theCards.push(card);
+              }
+            });
+        automated ? setpriorityCard(theCards) : setData(theCards);
+      };
 
     const [cardActive, setCardActive] = useState("")
     console.log(cardActive)
@@ -25,80 +65,52 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+
       <main className={`${styles.main} ${inter.className}`}>
+
         { (cardActive=="") ?
         <div className={styles.header}>
             <h1 className={styles.title}> Emergencies </h1>
             <div className={styles.line}/>
         </div> 
-        : <></>
-        }
+        : <></> }
+        <div>
+            <div className={styles.attended}>
+                <button onClick={() => setautomated(true)} className={styles.attendedselect}>
+                    <h3>Automated</h3>
+                </button>
+                <button
+                    onClick={() => setautomated(false)} className={styles.attendedselect}>
+                    <h3>Live Attended</h3>
+                </button>
+            </div>
+      </div>
 
-        <div className={styles.callwrapper}>
-            {calls.map( (call) => (
-
-                (cardActive==call.id || cardActive=="") ? 
-                <div className={cardActive==call.id ? `${styles.card} ${styles.cardactive}` : styles.card} onClick={() => 
-                {
-                    if (!(cardActive==call.id)) {
-                        setCardActive(call.id) 
-                    }
-                }}>
-                    { cardActive==call.id ? 
-
-
-
-                    <div> {/* fullscreen */}
-                        <div className={styles.header}>
-                            <div className={styles.titlewrapper}>
-                            <h1 className={styles.titleactive}> {call.name} </h1>
-                            <motion.img
-                            
-                                className={styles.exit}
-                                onClick={() => setCardActive("")}
-                                src={"/cross.png"}
-                                whileHover={{ scale: 1.1, rotate: -90 }}
-                                whileTap={{ scale: 0 }}/> 
-                            </div>
-                            <div className={styles.line}/>
-                        </div> 
-                        <div className={styles.bodyactive}> 
-                            <p> Status: <span>{call.status}</span></p>
-                            <p> Call Started: <span>{call.time}</span></p>
-                            <p> Location: <span>{call.location}</span></p>
-                        </div>
-                        <div className={styles.log}>
-                            <h2> Log </h2>
-                            <div className={styles.line}/>
-                            <div className={styles.logbody}>
-                                {call.log.map( (log) => (
-                                    <p> {log.time} &nbsp; {log.speaker}: <span>{log.text}</span> </p> 
-                                ))}
-                            </div>
-
-                        </div>
-                        <h1 className={styles.dispatchtitle}> Dispatch </h1>
-                        <div className={styles.line}/>
-
-                        <div className={styles.dispatch}>
-                            <motion.div className={`${styles.dispatchicon} ${styles.icon1}`} whileHover={{ scale: 1.1, }}> <p>TORONTO FIRE SERVICES</p> </motion.div>
-                            <motion.div className={`${styles.dispatchicon} ${styles.icon2}`} whileHover={{ scale: 1.1, }}> <p>TORONTO POLICE SERVICES</p> </motion.div>
-                            <motion.div className={`${styles.dispatchicon} ${styles.icon3}`} whileHover={{ scale: 1.1, }}> <p>TORONTO PARAMEDIC SERVICES</p> </motion.div>
-                        </div>
-                    </div> 
-
-
-                    
-                    : <div> {/* card not fullscreen */}
-                        <h2> {call.name} </h2>
-                        <p> Status: <span>{call.status}</span></p>
-                        <p> Call Started: <span>{call.time}</span></p>
-                        <p> Location: <span>{call.location}</span></p>
-                    </div>
-                    }
-                </div> : <></>
+    <div className={styles.emergencies}>
+        {automated
+          ? labels.map((label) => (
+              <PriorityCards
+                priority={label}
+                priorityCard={priorityCard}
+                setpriorityCard={setpriorityCard}
+                updateLabel={updateLabel}
+                selectedCard={selectedCard}
+                setSelectedCard={setSelectedCard}
+              />
+            ))
+          : labels.map((label) => (
+              <PriorityCards
+                priority={label}
+                priorityCard={data}
+                setpriorityCard={setData}
+                updateLabel={updateLabel}
+                selectedCard={selectedCard}
+                setSelectedCard={setSelectedCard}
+              />
             ))}
-        </div>    
+      </div>
+   
       </main>
     </div>
   )
